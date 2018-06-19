a21e430e1ce4c70ffb613df8509a50da50a1401c
Phil Boyd <phil.boyd@live.com>
2018-06-19 12:20:02 -0500

    lesson 008/009: implemented LoginAction

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,18 @@
+import { Action } from '@ngrx/store';
+import { User } from '../model/user.model';
+
+export enum AuthActionTypes {
+    LoginAction = '[Login] Action',
+    LogoutAction = '[Logout] Action'
+}
+
+export class LoginAction implements Action {
+    readonly type = AuthActionTypes.LoginAction;
+    constructor(public payload: { user: User }) {}
+}
+
+export class LogoutAction implements Action {
+    readonly type = AuthActionTypes.LogoutAction;
+}
+
+export type AuthActions = LoginAction | LogoutAction;
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,41 +1,47 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import {Store} from "@ngrx/store";
+import { Store } from '@ngrx/store';
 
-import {AuthService} from "../auth.service";
-import {tap} from "rxjs/operators";
-import {noop} from "rxjs";
-import {Router} from "@angular/router";
+import { AuthService } from '../auth.service';
+import { tap } from 'rxjs/operators';
+import { noop } from 'rxjs';
+import { Router } from '@angular/router';
+import { AppState } from '../../reducers';
+import { LoginAction } from '../auth.actions';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    form: FormGroup;
 
-  form: FormGroup;
+    constructor(
+        private fb: FormBuilder,
+        private auth: AuthService,
+        private router: Router,
+        private _store: Store<AppState>
+    ) {
+        this.form = fb.group({
+            email: ['test@angular-university.io', [Validators.required]],
+            password: ['test', [Validators.required]]
+        });
+    }
 
-  constructor(
-      private fb:FormBuilder,
-      private auth: AuthService,
-      private router:Router) {
+    ngOnInit() {}
 
-      this.form = fb.group({
-          email: ['test@angular-university.io', [Validators.required]],
-          password: ['test', [Validators.required]]
-      });
-
-  }
-
-  ngOnInit() {
-
-  }
-
-  login() {
-
-  }
-
-
+    login() {
+        const login = this.form.value;
+        this.auth
+            .login(login.email, login.password)
+            .pipe(
+                tap(user => {
+                    this._store.dispatch(new LoginAction({user}));
+                    this.router.navigateByUrl('/courses');
+                })
+            )
+            .subscribe(noop, () => alert('Login Failed'));
+    }
 }
