f7527d9ce7ff44bde6e532231cbaac996504d2e8
Phil Boyd <phil.boyd@live.com>
2018-06-19 15:56:12 -0500

    lesson 10 - the first reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,19 +1,40 @@
 import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
+    ActionReducer,
+    ActionReducerMap,
+    createFeatureSelector,
+    createSelector,
+    MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { User } from '../model/user.model';
+import { AuthActionTypes } from '../auth/auth.actions';
 
+interface AuthState {
+    loggedIn: boolean;
+    user: User;
+}
+
+// this is the app model - properties for each part of the application
 export interface AppState {
+    auth: AuthState;
+}
 
+function authReducer(state: AuthState, action): AuthState {
+    switch (action.type) {
+        case AuthActionTypes.LoginAction:
+            return {
+                loggedIn: true,
+                user: action.payload.user
+            };
+        default:
+            return state;
+    }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+    auth: authReducer
 };
 
-
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+    ? []
+    : [];
