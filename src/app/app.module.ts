4a03d5ed92ad465b974ba79184a7c49fb962c633
Phil Boyd <phil.boyd@live.com>
2018-06-22 13:30:26 -0500

    lesson 21: router-store for time traveling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import {
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, metaReducers } from './reducers';
 import { AuthGuard } from './auth/auth.guard';
+import { CustomSerializer } from './shared/custom-serializer';
 
 const routes: Routes = [
     {
@@ -54,9 +55,10 @@ const routes: Routes = [
         AuthModule.forRoot(),
         StoreModule.forRoot(reducers, { metaReducers }),
         !environment.production ? StoreDevtoolsModule.instrument() : [],
-        EffectsModule.forRoot([])
+        EffectsModule.forRoot([]),
+        StoreRouterConnectingModule.forRoot({ stateKey: 'router' })
     ],
-    providers: [],
+    providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
